36995ab6b89d5d28da08d85031532d1c576ac445
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-05-10 18:06:25 +0200

    Added the first unit tests

---
+++ tests/CodefogTagsBundleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Codefog\TagsBundle\Test;
+
+use Codefog\TagsBundle\CodefogTagsBundle;
+use Codefog\TagsBundle\DependencyInjection\Compiler\ManagerPass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CodefogTagsBundleTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        static::assertInstanceOf(CodefogTagsBundle::class, new CodefogTagsBundle());
+    }
+
+    public function testRegisterCompilerPass()
+    {
+        $container = new ContainerBuilder();
+        $bundle = new CodefogTagsBundle();
+        $bundle->build($container);
+        $found = false;
+
+        foreach ($container->getCompiler()->getPassConfig()->getPasses() as $pass) {
+            if ($pass instanceof ManagerPass) {
+                $found = true;
+                break;
+            }
+        }
+
+        static::assertTrue($found);
+    }
+}
