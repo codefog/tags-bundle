804070b761f48f8aebe872b941821ce669e6a5d2
Ralf Baussenwein <14016098+rabauss@users.noreply.github.com>
2022-03-15 08:46:30 +0100

    Fix undefined array key deleteConfirm (#33)

--- src/Resources/contao/dca/tl_cfg_tag.php
+++ src/Resources/contao/dca/tl_cfg_tag.php
@@ -62,7 +62,7 @@ $GLOBALS['TL_DCA']['tl_cfg_tag'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_cfg_tag']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_cfg_tag']['show'],
