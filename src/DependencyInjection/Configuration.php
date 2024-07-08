5e964edde1c0f2bdf4acc7dee0c5882f12132b31
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-07-08 07:26:01 +0100

    Add compatibility for `symfony/config` `7.x` (#41)
    * Add compatibility for `symfony/config` `7.x`
    
    * add dependency

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('codefog_tags');
 
