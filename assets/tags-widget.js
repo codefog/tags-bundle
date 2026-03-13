c7079b9cd13b05e33b1c5db3df96b694c370331d
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2026-03-13 09:35:53 +0100

    Stylelint + ESlint

--- assets/tags-widget.js
+++ assets/tags-widget.js
@@ -1,34 +1,34 @@
 import './tags-widget.scss';
 
-import {Application, Controller} from '@hotwired/stimulus';
+import { Application, Controller } from '@hotwired/stimulus';
 import TomSelect from 'tom-select';
 
 const application = Application.start();
 application.debug = process.env.NODE_ENV === 'development';
 application.register('codefog--tags-widget', class extends Controller {
-    static targets = ['input']
+    static targets = ['input'];
 
     static values = {
         config: {
             type: Object,
             default: {},
-        }
-    }
+        },
+    };
 
     connect() {
-        this.tomSelect = new TomSelect(this.inputTarget, this.#getOptions())
+        this.tomSelect = new TomSelect(this.inputTarget, this.#getOptions());
     }
 
     disconnect() {
-        this.tomSelect.destroy()
+        this.tomSelect.destroy();
     }
 
     add(event) {
-        this.tomSelect.addItem(event.target.value)
+        this.tomSelect.addItem(event.target.value);
     }
 
     remove(event) {
-        this.tomSelect.removeItem(event.target.value)
+        this.tomSelect.removeItem(event.target.value);
     }
 
     #getOptions() {
@@ -42,12 +42,12 @@ application.register('codefog--tags-widget', class extends Controller {
             render: {
                 option_create: (data, escape) => `<div class="create">${config.addLabel} <strong>${escape(data.input)}</strong>&hellip;</div>`,
                 item: (data, escape) => `<div>${escape(data.text)}<button type="button" class="cfg-tags-widget__remove" value="${data.value}" aria-label="${config.removeLabel} ${escape(data.text)}" data-action="click->codefog--tags-widget#remove:prevent">${config.removeLabel}</button></div>`,
-                no_results: (data,escape) => `<div class="no-results">${config.noResultsLabel}</div>`,
-            }
+                no_results: () => `<div class="no-results">${config.noResultsLabel}</div>`,
+            },
         };
 
         if (config.allowCreate) {
-            options.create = input => ({ value: input, text: input });
+            options.create = (input) => ({ value: input, text: input });
         }
 
         if (config.maxItems) {
